c6a0cbeb6cdd35b9f3a83137b4d13552dcbe5740
Camilo Contreras <contreras.camilo32@gmail.com>
2022-10-23 13:32:36 -0300

    directory reloaded

---
+++ Fuentes/legacy/CharmexProduccionesSite/src/main/java/com/charmex/charmexproducciones/utils/ConstantesEmail.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.charmex.charmexproducciones.utils;
+
+/**
+ *
+ * @author Camilo
+ */
+public class ConstantesEmail {
+
+    /**
+     * EMAIL
+     */
+    public static final String EMAIL_SENDER = "soporte@charmexproducciones.cl";
+    
+    public static final String EMAIL_USUARIO = "soporte.charmexproducciones@gmail.com";
+    public static final String EMAIL_CLAVE = "maraca123";
+
+}
